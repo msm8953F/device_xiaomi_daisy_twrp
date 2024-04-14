65882c038410a8d694cb1d45c98dea910f1c9d89
ZorEl212 <yabsirad212@proton.me>
2024-04-14 04:15:56 +0000

    libinit: refactor the whole thing for compatablity with a12.1 some inclusions are no longer defined on android 12
    Signed-off-by: ZorEl212 <yabsirad212@proton.me>

--- init/init_sakura.cpp
+++ init/init_sakura.cpp
@@ -9,17 +9,34 @@
 #include <fcntl.h>
 #include <android-base/logging.h>
 #include <android-base/properties.h>
+#include <sys/resource.h>
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
 
-#include "property_service.h"
-#include "log.h"
 
-namespace android {
-namespace init {
+using android::base::GetProperty;
 
-void vendor_load_properties() {
-    property_set("ro.bootimage.build.date.utc", "1514797200");
-    property_set("ro.build.date.utc", "1514797200");
+void property_override(const std::string& name, const std::string& value)
+{
+    size_t valuelen = value.size();
+
+    prop_info* pi = (prop_info*) __system_property_find(name.c_str());
+    if (pi != nullptr) {
+        __system_property_update(pi, value.c_str(), valuelen);
+    }
+    else {
+        int rc = __system_property_add(name.c_str(), name.size(), value.c_str(), valuelen);
+        if (rc < 0) {
+            LOG(ERROR) << "property_set(\"" << name << "\", \"" << value << "\") failed: "
+                       << "__system_property_add failed";
+        }
+    }
 }
 
-}  // namespace init
-}  // namespace android
+void vendor_load_properties() {
+    property_override("ro.bootimage.build.date.utc", "1514797200");
+    property_override("ro.build.date.utc", "1514797200");
+}
+
+  // namespace init
+  // namespace android
