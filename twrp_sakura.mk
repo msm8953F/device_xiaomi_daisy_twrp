84fc99ff3899a8c73123f91aff4dc9124ac4b43b
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2025-03-05 23:40:35 +0600

    Updated to daisy

--- twrp_sakura.mk
+++ twrp_sakura.mk
@@ -5,7 +5,7 @@
 #
 
 # Release name
-PRODUCT_RELEASE_NAME := sakura
+PRODUCT_RELEASE_NAME := daisy
 
 
 # Inherit from those products. Most specific first.
@@ -13,15 +13,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit from hardware-specific part of the product configuration
-$(call inherit-product, device/xiaomi/sakura/device.mk)
+$(call inherit-product, device/xiaomi/daisy/device.mk)
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := sakura
-PRODUCT_NAME := twrp_sakura
+PRODUCT_DEVICE := daisy
+PRODUCT_NAME := twrp_daisy
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := Redmi 6 pro
+PRODUCT_MODEL := Mi A2 Lite
 PRODUCT_MANUFACTURER := Xiaomi
 
